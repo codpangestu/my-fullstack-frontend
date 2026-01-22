29da5c2301154807893c612adb6fa005e73f7ba9
codpangestu <arvstu.id@gmail.com>
2026-01-22 16:22:08 +0700

    fix: allow railway host in vite preiview 3

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
   preview: {
-    allowedHosts: "my-fullstack-frontend-production.up.railway.app",
+    host: true,
+    port: 8080,
+    allowedHosts: ['.railway.app'],
   },
 })
